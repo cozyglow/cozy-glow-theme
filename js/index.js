59b80ccfc28b2c45aa15e0bd231bd4c8e720805c
Kristian Matthews <kristian.matthews@me.com>
2021-02-09 22:31:12 +0000

    Add global navbar plugin

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Import Bootstrap Kit's bundle (all of Bootstrap's JS + Popper.js dependency)
+
+import '@epickris/bootstrap-kit/dist/js/bootstrap-kit.bundle';
+
+import NavbarGlobal from './navbar-global';
+
+export {
+  NavbarGlobal
+};
